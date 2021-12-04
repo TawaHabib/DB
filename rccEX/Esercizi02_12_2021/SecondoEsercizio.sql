384fc032e314a7d65c3686c5186c9a4ff2c562ed
TawaHabib <user@pc-lenovo.com>
2021-12-04 14:03:49 +0100

    esercizi completati

--- rccEX/Esercizi02_12_2021/SecondoEsercizio.sql
+++ rccEX/Esercizi02_12_2021/SecondoEsercizio.sql
@@ -3,14 +3,42 @@
 	2	il numero UNOSUP di dipartimenti nei quali un solo dipendente svolge quella mansione
 	3	il numero DUESUP di dipartimenti nei quali almeno due dipendente svolgono quella mansione;
 Elencare tutte le MANSIONE, assieme a ZEROSUP, UNOSUP e DUESUP.
-richiesta ambigua? sup --> si riferisce a superiore?? <non ho capito la richiesta>
 */
-###########TODO##########
-###########TODO##########
-###########TODO##########
-###########TODO##########
-#zero sup
-select *
-from prova.dipendenti;
-	(select distinct Mansione, num_dipart
-	from prova.dipendenti)
+SELECT		T0.MANSIONE,
+			coalesce(T1.ZEROSUP, "0") AS ZEROSUP, 
+			coalesce(T2.UNOSUP, "0") AS UNOSUP, 
+            coalesce(T3.DUESUP, "0") AS DUESUP
+FROM 	
+		(###########----MANSIONI----##########
+        SELECT DISTINCT MANSIONE FROM prova.dipendenti
+        ) AS T0
+LEFT JOIN 
+		(###########----ZEROSUP----##########
+		SELECT MANSIONE,  COUNT(NUM_DIPART) AS ZEROSUP
+		FROM 
+				(SELECT NUM_DIPART FROM prova.dipart)AS T1,
+				(SELECT DISTINCT MANSIONE FROM prova.dipendenti) AS T2
+		where (NUM_DIPART,MANSIONE)
+			NOT IN (SELECT DISTINCT  NUM_DIPART, MANSIONE FROM prova.dipendenti where NUM_DIPART IS NOT NULL)
+		group by MANSIONE
+        )AS T1 ON T0.MANSIONE=T1.MANSIONE 
+LEFT JOIN
+		(###########----UNOSUP----##########
+        SELECT MANSIONE, COUNT(DISTINCT NUM_DIPART) AS UNOSUP
+		FROM(
+				SELECT  MANSIONE,NUM_DIPART
+				FROM prova.dipendenti
+				group by MANSIONE,NUM_DIPART 
+				having count(NUM_DIPART)=1) AS T1
+		group by MANSIONE
+            )AS T2 ON T0.MANSIONE=T2.MANSIONE
+LEFT JOIN
+		(###########----DUESUP----##########
+		SELECT MANSIONE, COUNT(DISTINCT NUM_DIPART) AS DUESUP
+		FROM(
+				SELECT  MANSIONE,NUM_DIPART
+				FROM prova.dipendenti
+				group by MANSIONE,NUM_DIPART 
+				having count(NUM_DIPART)>1) AS T1
+		group by MANSIONE)AS T3 ON T0.MANSIONE=T3.MANSIONE
+ORDER BY MANSIONE
--- rccEX/Esercizi02_12_2021/primoEsercizio.sql
+++ rccEX/Esercizi02_12_2021/primoEsercizio.sql
@@ -4,15 +4,17 @@
 		tutti i prodotti che sono nelle forniture.
 Elencare poi tutti i dati del FORNITORE e, per il caso a) la stringa “assente”; per il caso b) NPRINF. */
 select *
-from (
-		##B
-		select Fcod as cod,count(distinct Prcod) as quanti
+from 
+	( ####### --A-- UNION --B--
+		(#######A
+		SELECT distinct cod, "assente" as NPRINF
+		FROM prova.fornitori
+		where cod not in (select Fcod From prova.forniture))
+		union
+        (#######B
+		select Fcod as cod,count(distinct Prcod) as NPRINF
 		from prova.forniture
 		group by FCOD
-		having quanti<(select count(distinct pcod)/2 from prova.forniture)
-		union
-		##A
-		SELECT distinct cod, "assente" 
-		FROM prova.fornitori
-		where cod not in (select Fcod From prova.forniture)
-		) as t1 join prova.fornitori as t2 on (t1.cod=t2.cod)
+		having count(distinct Prcod)<(select count(distinct pcod)/2 from prova.forniture))
+	) as t1
+join prova.fornitori as t2 on (t1.cod=t2.cod)
