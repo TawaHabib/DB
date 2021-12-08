4591446686fd7b7e5cb60393f11448fc4bb869e2
TawaHabib <user@pc-lenovo.com>
2021-12-08 16:56:13 +0100

    esercizi completati

--- rccEX/Esercizi02_12_2021/SecondoEsercizio.sql
+++ rccEX/Esercizi02_12_2021/SecondoEsercizio.sql
@@ -14,31 +14,39 @@ FROM
         ) AS T0
 LEFT JOIN 
 		(###########----ZEROSUP----##########
-		SELECT MANSIONE,  COUNT(NUM_DIPART) AS ZEROSUP
+        SELECT MANSIONE,  COUNT(NUM_DIPART) AS ZEROSUP
 		FROM 
-				(SELECT NUM_DIPART FROM prova.dipart)AS T1,
-				(SELECT DISTINCT MANSIONE FROM prova.dipendenti) AS T2
-		where (NUM_DIPART,MANSIONE)
-			NOT IN (SELECT DISTINCT  NUM_DIPART, MANSIONE FROM prova.dipendenti where NUM_DIPART IS NOT NULL)
+				(#CASI TEORICI 
+				(#TUTTI I DIPARTIMENTI
+					SELECT NUM_DIPART 
+					FROM prova.dipart)AS T1,				
+				(#TUTTE LE MANSIONI
+					SELECT DISTINCT MANSIONE 
+					FROM prova.dipendenti) AS T2)	
+		where 	(NUM_DIPART,MANSIONE)	NOT IN
+				(#CASI REALI DI MANSIONI E DIPARTIMENTI
+						SELECT DISTINCT  NUM_DIPART, MANSIONE 
+						FROM prova.dipendenti 
+						where NUM_DIPART IS NOT NULL)
 		group by MANSIONE
         )AS T1 ON T0.MANSIONE=T1.MANSIONE 
 LEFT JOIN
 		(###########----UNOSUP----##########
-        SELECT MANSIONE, COUNT(DISTINCT NUM_DIPART) AS UNOSUP
+		SELECT MANSIONE, count(NUM_DIPART) AS UNOSUP
 		FROM(
-				SELECT  MANSIONE,NUM_DIPART
+				SELECT  MANSIONE,NUM_DIPART, count(Matricola) as UNOSUP
 				FROM prova.dipendenti
-				group by MANSIONE,NUM_DIPART 
-				having count(NUM_DIPART)=1) AS T1
+				group by MANSIONE,NUM_DIPART
+                having count(Matricola)=1)AS T1 
 		group by MANSIONE
             )AS T2 ON T0.MANSIONE=T2.MANSIONE
 LEFT JOIN
 		(###########----DUESUP----##########
-		SELECT MANSIONE, COUNT(DISTINCT NUM_DIPART) AS DUESUP
+		SELECT MANSIONE, count(NUM_DIPART) AS DUESUP
 		FROM(
-				SELECT  MANSIONE,NUM_DIPART
+				SELECT  MANSIONE,NUM_DIPART, count(Matricola) as DUESUP
 				FROM prova.dipendenti
-				group by MANSIONE,NUM_DIPART 
-				having count(NUM_DIPART)>1) AS T1
+				group by MANSIONE,NUM_DIPART
+                having count(Matricola)>1)AS T1 
 		group by MANSIONE)AS T3 ON T0.MANSIONE=T3.MANSIONE
 ORDER BY MANSIONE
